54979a46ce671ab370cf8c2788e56f31a1f80467
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-12-04 21:47:17 +0200

    Remove Sidebar component from RootLayout for streamlined dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,13 @@
-'use client';
+"use client";
 import { Inter } from "next/font/google";
 import "../../app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar";
-import { DataProvider } from "@/context/DataContext"; 
+import { DataProvider } from "@/context/DataContext";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function RootLayout({
   children,
@@ -40,7 +38,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <div className="flex flex-col h-screen md:flex-row">
-              <Sidebar />
               <div className="flex-1 flex flex-col overflow-hidden">
                 <Header />
                 <main className="flex-1 overflow-x-hidden overflow-y-auto bg-background p-4">
