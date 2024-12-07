c38eabd4c009f384e481eeec32269b8b027e072d
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-12-07 20:34:35 +0200

    Add user data fetching and handling in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import "../../app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { DataProvider } from "@/context/DataContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,17 +15,45 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // Check for the token in localStorage
-    const token = localStorage.getItem("token");
+    const fetchUserData = async () => {
+      const token = localStorage.getItem("token");
 
-    // If no token is found, redirect to login page
-    if (!token) {
-      router.push("/login");
-    }
+      if (!token) {
+        console.warn("No token found, redirecting to login");
+        router.push("/login");
+        return;
+      }
+
+      try {
+        const response = await fetch("/api/user", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          console.error("Failed to fetch user data:", errorData);
+          throw new Error(response.statusText);
+        }
+
+        const data = await response.json();
+        console.log("Fetched user data:", data); // Log the user data
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        router.push("/login");
+      }
+    };
+
+    fetchUserData();
   }, [router]);
-
+  console.log(user);
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -39,7 +67,7 @@ export default function RootLayout({
           >
             <div className="flex flex-col h-screen md:flex-row">
               <div className="flex-1 flex flex-col overflow-hidden">
-                <Header />
+                {user && <Header user={user} />}
                 <main className="flex-1 overflow-x-hidden overflow-y-auto bg-background p-4">
                   {children}
                 </main>
